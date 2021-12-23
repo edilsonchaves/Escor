6841c0b40291191682949e62b465926bc1f13921
Alan Marinho <kgometalenator@gmail.com>
2021-12-23 07:45:36 -0300

    Script movimentos Player

---
+++ Escor/Assets/Escor/Player/Grounded.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+
+    
+    Movement Player;
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject.GetComponent<Movement>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collisor)
+    {
+        if(collisor.gameObject.tag == "ground")
+        {
+            Player.isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collisor)
+    {
+        if(collisor.gameObject.tag == "ground")
+        {
+            Player.isJumping = true;
+        }
+    }
+}
