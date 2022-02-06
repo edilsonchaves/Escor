f8d683f421f9b2b21a47aea0339ce5ab45f07a42
edilsonchaves <edilson.montenegro@gmail.com>
2022-02-06 13:23:20 -0300

    Language Apply

--- Escor/Assets/Escor/Scripts/LanguageAdaptScene.cs
+++ Escor/Assets/Escor/Scripts/LanguageAdaptScene.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class LanguageAdaptScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    // 
+    void Start()
+    {
+        ManagerEvents.GameConfig.ChangedLanguage(Manager_Game.Instance.saveGameData.LanguageSelect);
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
+
