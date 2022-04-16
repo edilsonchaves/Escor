5c33ce10e97f38914115a38cb0ca8839de269b38
JHDsBR <Jesse2013.jsc@gmail.com>
2022-04-16 01:23:12 -0300

    Criado a plataforma
    Já coloquei a plataforma na casa, e ela só cai quando há dois objetos em cima.

--- Escor/Assets/Jessé/Scripts/DropPlatform.cs
+++ Escor/Assets/Jessé/Scripts/DropPlatform.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class DropPlatform : MonoBehaviour
 {
 
-    public enum PlatformTypes {TwoBlocks, ThreeBlocks, ThreeBlocksWithShadow};
+    public int numberOfPeoplesToDrop=1;
+
+    public enum PlatformTypes {TwoBlocks, ThreeBlocks, ThreeBlocksWithShadow, SevenBlocks};
     public PlatformTypes PlatformType;
 
     public string tagOfPlayer="Player";
@@ -21,9 +23,10 @@ public class DropPlatform : MonoBehaviour
         myAnimator = GetComponent<Animator>();
     }
 
+
     void Update()
     {
-        if(!alreadyAnimade && abovePlatformManager.isAbove)
+        if(!alreadyAnimade && abovePlatformManager.numberOfAbove >= numberOfPeoplesToDrop)
         {
             alreadyAnimade = true;
 
@@ -37,6 +40,9 @@ public class DropPlatform : MonoBehaviour
                     break;
                 case (PlatformTypes.ThreeBlocksWithShadow):
                     myAnimator.Play("ChaoQueCai3Start", -1, 0);
+                    break;
+                case (PlatformTypes.SevenBlocks):
+                    myAnimator.Play("ChaoQueCai7Start", -1, 0);
                     break;
             }
         }
@@ -64,9 +70,10 @@ public class DropPlatform : MonoBehaviour
     //     }
     // }
 
-
     void DestroyMe()
     {
+        // não pode destruir antes de remover o player ou javali, senão irá destruir eles também
+        // o melhor a se fazer é apenas deixa a plataforma invisivel
         Destroy(gameObject, .2f);
     }
 
