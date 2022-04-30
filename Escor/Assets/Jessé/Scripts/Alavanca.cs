e1ebdd4cf9b83756b6bf2428f59b692876d03d09
JHDsBR <Jesse2013.jsc@gmail.com>
2022-04-30 14:33:21 -0300

    Script
    Fiz um script para o foco da câmera em objetos específicos

--- Escor/Assets/Jessé/Scripts/Alavanca.cs
+++ Escor/Assets/Jessé/Scripts/Alavanca.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Alavanca : MonoBehaviour
 {
 
-    [SerializeField] private string tagOfPlayer; 
+    [SerializeField] private string tagOfPlayer;
     [SerializeField] private KeyCode actionButton;
     [SerializeField] private Animator myAnimator;
-    [SerializeField] private Animator[] portoes;
-    
+    [SerializeField] private GameObject[] portoes;
+
+    public bool openInSequence=false; // abre os portões em sequencia casa exista mais de um
+
+    public VcamFocusObject vcamFocusObject;
+
     private Movement mvt;
-    
+
     bool alreadyTriggered=false;
 
 
@@ -20,11 +24,19 @@ public class Alavanca : MonoBehaviour
         myAnimator = GetComponent<Animator>();
     }
 
+
     void OnTriggerStay2D(Collider2D col)
     {
         if(col.tag == tagOfPlayer && Input.GetKey(actionButton) && !alreadyTriggered)
         {
             myAnimator.Play("alavanca", -1, 0);
+
+            vcamFocusObject.StartFocus(portoes);
+
+            // vcamFocusObject.StartFocus(portoes, false);
+            // vcamFocusObject.StartFocus(new GameObject[]{portoes[0]});
+            // vcamFocusObject.StartFocus(new GameObject[]{portoes[0]}, false);
+
             col.transform.position = new Vector3(transform.position.x-0.18f, col.transform.position.y, col.transform.position.z);
             col.transform.eulerAngles = Vector3.zero;
             mvt = col.GetComponent<Movement>();
@@ -36,10 +48,30 @@ public class Alavanca : MonoBehaviour
     // quem está chamando é a própria animação da alavanca
     void OpenAllDoors()
     {
-        foreach(Animator portao in portoes)
+        StartCoroutine(OpenAllDoors_());
+    }
+
+
+    IEnumerator OpenAllDoors_()
+    {
+        foreach(GameObject goPortao in portoes)
         {
+            Animator portao = goPortao.GetComponent<Animator>();
             portao.Play("PortaoAbrindoStart", -1, 0);
+
+            if(openInSequence)
+            {
+                yield return new WaitUntil(() => (portao.GetCurrentAnimatorStateInfo(0).IsName("PortaoAbrindoStart"))); // espera a animação mudar para 'PortaoAbrindoStart'
+                yield return new WaitUntil(() => (portao.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)); // espera a animação chegar no final
+                vcamFocusObject.GoToNextStep();
+                // vcamFocusObject.Finish();
+            }
+
+
+            yield return null;
         }
+
+        // vcamFocusObject.Finish();
     }
 
 
