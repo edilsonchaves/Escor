664e1bc66126a194de3b491582417647dbe994b3
JHDsBR <Jesse2013.jsc@gmail.com>
2022-05-09 18:25:18 -0300

    Esconder local dentro da casa 1
    Coloquei uma parede que escode o interior da casa, e só revela o que há por trás quando o jogador entra.

--- Escor/Assets/Jessé/Scripts/MovePlataform.cs
+++ Escor/Assets/Jessé/Scripts/MovePlataform.cs
@@ -19,7 +19,7 @@ public class MovePlataform : MonoBehaviour
 
     [HideInInspector] Vector2 myVelocity;
 
-    [HideInInspector] public bool esperarAtivador; // a propria alavanca que define essa variável
+    [HideInInspector] public bool esperarAtivador=false; // a propria alavanca que define essa variável
 
     // Gizmos - Desenvolvimento
     void OnDrawGizmos()
