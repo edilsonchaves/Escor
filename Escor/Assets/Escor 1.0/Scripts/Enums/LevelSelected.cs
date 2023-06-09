e477638b11543579a5ef0ce899e5be8af09613cc
edilsonchaves <edilson.montenegro@gmail.com>
2023-06-08 22:20:57 -0300

    Save Load Estrutura de funcionamento
    Save Load Estrutura de funcionamento

---
+++ Escor/Assets/Escor 1.0/Scripts/Enums/LevelSelected.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelected
+{
+    public enum LevelEnum {Abertura, Cena_jogo, Introducao, Level_1, Level_2,Level_tutorial, Menu, Cena_teste }
+}
